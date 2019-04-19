8940a823db9f0d23a22a01367f20119a352716d2
Luke Turner <luke@lukedturner.com>
2019-04-19 13:45:56 +1000

    added associations and enum to db

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,8 @@
 class Breed < ApplicationRecord
+    has_many :listings
+    #A breed can have many listings,
+    #there can be many anaconda listings for example.
+
+    #enum --> a set of named key value pairs
+    
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,12 @@
 class Listing < ApplicationRecord
+  #using the foreign key "breed_id"
   belongs_to :breed
+  
+  #make a list of named key value pairs 
+  # in an enum --> useful for when values
+  # must be something in particular
+  enum sex: { female:0, male:1 }
+  
+
+
 end
